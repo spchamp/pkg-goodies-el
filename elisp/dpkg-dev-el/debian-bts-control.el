08ba9c2627423759f88d1e3d4f48108dd8fa9a38
psg <psg>
2003-05-12 15:47:13 +0000

    tweak copyright

--- elisp/dpkg-dev-el/debian-bts-control.el
+++ elisp/dpkg-dev-el/debian-bts-control.el
@@ -7,7 +7,7 @@
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 ;;
-;; debian-changelog-mode.el is distributed in the hope that it will be useful,
+;; debian-bts-mode.el is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
