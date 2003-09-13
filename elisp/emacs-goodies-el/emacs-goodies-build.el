48ef4038a3e0b0fa0e32229eeda66046d9d89d55
psg <psg>
2003-09-13 00:50:56 +0000

    Maintenance tool. Makes emacs-goodies-custom.el.

--- elisp/emacs-goodies-el/emacs-goodies-build.el
+++ elisp/emacs-goodies-el/emacs-goodies-build.el
@@ -1,3 +1,45 @@
+;;; emacs-goodies-build.el --- emacs-goodies-el maintance code
+;; Copyright (C) 2003 Peter S. Galbraith
+
+;; This package is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This package is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;; ----------------------------------------------------------------------------
+
+;;; Commentary:
+;; 
+;; A few very rough functions to help with the maintenance of the
+;; emacs-goodies-el pacakge.  None of this is meant for everyday use.
+;;
+;; I created `insert-missing-autoloads' to see what files didn't have
+;; `;;;###autoload' markers for autoloads in emacs-goodies-el.el.
+;;
+;; Then I created `delete-tagged-autoloads' the remove a bunch of autoloads
+;; that were entered manually in emacs-goodies-el.el since they can be
+;; automatically generated.
+;;
+;; `insert-defgroup' and `insert-defgroup-dired' are used to automatically
+;; make the file `emacs-goodies-custom.el' which loads a modified version
+;; of all `defgroup' declarations from all files.  Go into dired for this
+;; directory and run `insert-defgroup-dired' from the line before the first
+;; listed file.  It will generate a buffer with `emacs-goodies-custom.el' from all files.
+
+;;; History:
+;; 
+
+;;; Code:
 (defun insert-missing-autoloads ()
   "Scan emacs-goodies-el.el for autoloads and check if there are in files."
   (interactive)
@@ -15,10 +57,11 @@
             (message "Inserted for %s in %s" command efile)
             (save-buffer)))))))
 
-
-
 (defun delete-tagged-autoloads ()
-  "Scan emacs-goodies-el.el for autoloads and delete those that are marked."
+  "Scan emacs-goodies-el.el for autoloads and delete those that are marked.
+Those that already have a ;;###autoload marker string are deleted from
+emacs-goodies-el.el because a autoload file is automatically generated
+(i.e. emacs-goodies-loaddefs.el)."
   (interactive)
   (while (re-search-forward "(autoload '\\([^ ]+\\) \"\\(.*\\)\"" nil t)
     (let* ((start (match-beginning 0))
@@ -42,10 +85,8 @@
         (delete-region start (point))
         (message "***Deleting %s in %s" command efile)))))
 
-
-
 (defun insert-defgroup ()
-  "Scan buffer for defgroup statements and merge in emacs-goodies-custom.el
+  "Scan buffer for defgroup statements and merge in emacs-goodies-custom.el.
 Add a :link '
 Add a :group 'emacs-goodies-el"
   (interactive)
@@ -93,4 +134,6 @@ Add a :group 'emacs-goodies-el"
   (goto-char (point-max))
   (insert "(provide 'emacs-goodies-custom)\n"))
 
-      
+(provide 'emacs-goodies-build)
+
+;;; emacs-goodies-build.el ends here
